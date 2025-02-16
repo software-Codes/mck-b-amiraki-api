d6dc3b79a1cb523260c9d5a66468612c059949dc
CollinsMunene-Developer <collinsentrepreneur@gmail.com>
2025-02-16 13:58:57 +0300

    fix(azureStorage): correct Azure storage connection string and method name, improve uploadFile parameter usage

--- src/models/churchgallery/azureStorage.js
+++ src/models/churchgallery/azureStorage.js
@@ -1,36 +1,39 @@
-const {BlobServiceClient} = require('@azure/storage-blob');
-const {v4: uuidv4} =  require('uuid');
+const { BlobServiceClient } = require("@azure/storage-blob");
+const { v4: uuidv4 } = require("uuid");
 
+class AzureStorageService {
+  constructor() {
+    // Fixed method name
+    this.blobServiceClient = BlobServiceClient.fromConnectionString(
+      process.env.AZURE_STORAGE_CONNECTION_STRING
+    );
+    this.containerClient = this.blobServiceClient.getContainerClient(
+      process.env.AZURE_STORAGE_CONTAINER
+    );
+  }
 
-class AzureStorageService  {
-    constructor(){
-        this.blobServiceClient  = BlobServiceClient.froConnectionString(process.env.AZURE_STORAGE_CONNECTION_STRING);
-        this.containerClient  = this.blobServiceClient.getContainerClient(process.env.AZURE_STORAGE_CONTAINER_NAME);
-    }
+  async uploadFile(file, content_type) {
+    // Parameter is content_type
+    const extension = file.originalname.split(".").pop();
+    // Using correct parameter name
+    const blobName = `${content_type}/${uuidv4()}.${extension}`;
+    const blockBlobClient = this.containerClient.getBlockBlobClient(blobName);
 
-    async uploadFile(file , content_type){
-        const extension = file.originalname.split('.').pop();
-        const blobName = `${contentType}/${uuidv4()}.${extension}`;  
-            const blockBlobClient = this.containerClient.getBlockBlobClient(blobName);
+    const options = {
+      blobHTTPHeaders: {
+        blobContentType: file.mimetype,
+      },
+    };
 
-            const options = {
-                blobHTTPHeaders: {
-                    blobContentType: file.mimetype
-                }
-            };
+    await blockBlobClient.uploadData(file.buffer, options);
+    return blockBlobClient.url;
+  }
 
-            await blockBlobClient.uploadData(file.buffer, options);
-            return blockBlobClient.url;
-      }
-
-      
   async deleteFile(url) {
-    const blobName = url.split('/').pop();
+    const blobName = url.split("/").pop();
     const blockBlobClient = this.containerClient.getBlockBlobClient(blobName);
     await blockBlobClient.delete();
   }
 }
 
-module.exports = {
-    AzureStorageService
-};
+module.exports = { AzureStorageService };
